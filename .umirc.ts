2e7ce4c2acd078d89f6c75efdc69341d22f200fe
virgoone <me@virgo.one>
2021-04-13 10:03:07 +0800

    feat: 添加统计代码

--- .umirc.ts
+++ .umirc.ts
@@ -20,4 +20,7 @@ export default defineConfig({
       path: 'https://github.com/virgoone/interview',
     },
   ],
+  headScripts: [
+    { content: `!function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"JHRJXYlwhd65yctP",ck:"JHRJXYlwhd65yctP",hashMode: true});`, charset: 'utf-8' },
+  ],
 });
