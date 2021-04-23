53bdbb33f5410fcfca6a6ffa1e8dc4a950a7af11
virgoone <me@virgo.one>
2021-04-23 10:28:51 +0800

    feat: 增加meta信息

--- .umirc.ts
+++ .umirc.ts
@@ -23,4 +23,14 @@ export default defineConfig({
   headScripts: [
     { content: `!function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"JHRJXYlwhd65yctP",ck:"JHRJXYlwhd65yctP",hashMode: true});`, charset: 'utf-8' },
   ],
+  metas: [
+    {
+      name: 'keywords',
+      content: '面试,面试指个北,前端面试,react,js,css,html,前端工程化,面试指南,前端',
+    },
+    {
+      name: 'description',
+      content: '前端面试指南,常用面试题、手写代码',
+    },
+  ],
 });
